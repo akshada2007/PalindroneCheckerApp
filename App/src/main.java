be667750eaca5f88ebc1aa564d0afd6ad1df1ae9
Akshada1901 <akshada1901@gmail.com>
2026-03-05 23:05:15 +0530

    [Akshada2007] Added UC8-LinkedListBasedPalindromeChecker

--- App/src/main.java
+++ App/src/main.java
@@ -1,22 +1,5 @@
 import java.util.LinkedList;
 
-/**
- * =========================================================================
- * MAIN CLASS - UseCase8PalindromeCheckerApp
- * =========================================================================
- * * Use Case 8: Linked List Based Palindrome Checker
- * * Description:
- * This class checks whether a string is a palindrome 
- * using a LinkedList.
- * * Characters are added to the list and then compared
- * by removing elements from both ends:
- * * - removeFirst()
- * - removeLast()
- * * This demonstrates how LinkedList supports 
- * double-ended operations for symmetric validation.
- * * @author Developer
- * @version 8.0
- */
 public class UseCase8PalindromeCheckerApp {
 
     /**
